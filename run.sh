bad114f5f94b2e1b8fe67667e473b741b717d218
jimvanveenhuyzen <125078335+jimvanveenhuyzen@users.noreply.github.com>
2023-04-24 11:30:19 +0200

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+wget https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m11.txt
+wget https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m12.txt
+wget https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m13.txt
+wget https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m14.txt
+wget https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m15.txt
+
 python3 NURhandin3.py > NURhandin3problem1a.txt
 python3 NURhandin3.py > NURhandin3problem1b.txt
 
